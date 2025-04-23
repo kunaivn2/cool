f087e86f5827856495ba5cd8ae5d4240228e7dd8
kunaivn2 <kunaivn2@gmail.com>
2025-04-23 21:24:25 +0700

    Update gold.js

--- sgmodule/gold.js
+++ sgmodule/gold.js
@@ -5,9 +5,9 @@ const mapping = {
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
     obj = JSON.parse($response.body);
-obj.Attention = "";
-var locket02 = { is_sandbox: !1, ownership_type: "PURCHASED", billing_issues_detected_at: null, period_type: "normal", expires_date: "2099-12-18T01:04:17Z", grace_period_expires_date: null, unsubscribe_detected_at: null, original_purchase_date: "2024-04-12T01:04:18Z", purchase_date: "2024-04-12T01:04:17Z", store: "app_store" },
-    locket01 = { grace_period_expires_date: null, purchase_date: "2024-04-12T01:04:17Z", product_identifier: "com.locket02.premium.yearly", expires_date: "2099-12-18T01:04:17Z" };
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+var locket02 = { is_sandbox: !1, ownership_type: "PURCHASED", billing_issues_detected_at: null, period_type: "normal", expires_date: "2099-12-18T01:04:17Z", grace_period_expires_date: null, unsubscribe_detected_at: null, original_purchase_date: "2024-04-12T01:04:18Z", purchase_date: "2002-04-30T10:36:10Z", store: "app_store" },
+    locket01 = { grace_period_expires_date: null, purchase_date: "2002-04-30T10:36:10Z", product_identifier: "com.locket02.premium.yearly", expires_date: "2099-12-18T01:04:17Z" };
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
     let [e, s] = mapping[match];
